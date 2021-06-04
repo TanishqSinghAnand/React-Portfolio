ca8e49f070fd8084a89a3077fa3761bd3237117d
Tanishq Singh Anand <anandtanishqs@gmail.com>
2021-06-04 11:45:16 +0530

    Added build

---
+++ build/precache-manifest.e517701a63a401c8b880f470c3e06a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9acf6e0642e3909911a5b7343ce1795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15422a7daadccb9384c3",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f5514ba1504a2b17785b",
+    "url": "/static/js/2.03a7952c.chunk.js"
+  },
+  {
+    "revision": "ef7b90d5387b85ba011a924f2f792332",
+    "url": "/static/js/2.03a7952c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15422a7daadccb9384c3",
+    "url": "/static/js/main.924f83cc.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
